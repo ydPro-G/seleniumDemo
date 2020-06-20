4691d514a49d4bd7329ddabd04d57af02b0b4539
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-06-20 13:58:49 +0800

    操作元素的基本方法
    点击元素.click()
    
    输入内容：实例.相应元素.send_keys('内容')
    
    获取元素文本内容:
    变量 = 实例.方法（‘元素'）
    print(变量.text)
    获取元素属性的值：element.get_attribute(‘元素’)
    
     要获取整个元素对应的html文本内容可以使用element.get_attribute('outerHTML')
    
     要获取某个元素对应内部的HTML文本内容，可以使用element.attribute('innerHTML')

--- selenium/clickElement_7.py
+++ selenium/clickElement_7.py
@@ -15,6 +15,10 @@ clicks.find_element_by_id('su').click() # 指定元素，点击（click）
 from time import sleep # 等待时间2秒
 sleep(2)
 
+clicks.quit() # 退出
+
+
+
 
 
 
@@ -28,6 +32,51 @@ sleep(2)
 
 
 
+
+
 # 获取元素文本内容 ，通过————.text属性
 # 变量 = 实例.方法（元素）
 # print(变量.text)
+
+
+
+
+
+
+
+
+# 获取元素属性的值
+# 获取元素属性class的值，可以使用element.get_attribute('class')
+
+
+from selenium import webdriver
+
+wd = webdriver.Chrome()
+
+wd.get('http://cdn1.python3.vip/files/selenium/test3.html')
+
+element = wd.find_element_by_id('input1')
+print(element.get_attribute('value'))  #获取元素的属性值
+
+wd.quit()
+
+
+
+
+
+
+
+
+# 获取整个元素对应的HTML
+# 要获取整个元素对应的html文本内容可以使用element.get_attribute('outerHTML')
+# 要获取某个元素对应内部的HTML文本内容，可以使用element.attribute('innerHTML')
+print(element.get_attribute('outerHTML'))
+
+
+
+
+
+
+
+# 获取元素文本内容2
+# element.get_attribute('innerText')
